fd8fe9e7b28909ee3509d1bd0b649160da97427a
Sam Nobs <sam.nobs@resideo.com>
2025-10-09 12:40:45 +1300

    Reduce variable scope

--- include/logger.h
+++ include/logger.h
@@ -64,15 +64,15 @@ static const struct {
 } while(0)
 
 #define OCLEA_LOG_STREAM_THROTTLE(level, interval_chrono_duration, args) do { \
-    static int num_dropped = 0; \
-    static auto last_log_time = std::chrono::steady_clock::now() - interval_chrono_duration; \
-    auto now = std::chrono::steady_clock::now(); \
     if (OCLEA_LOG_CHECK_LEVEL(level)) { \
-        if (now - last_log_time >= interval_chrono_duration) { \
-            last_log_time = now; \
+       static int num_dropped = 0; \
+       static auto last_log_time = std::chrono::steady_clock::now() - interval_chrono_duration; \
+       auto now = std::chrono::steady_clock::now(); \
+       if (now - last_log_time >= interval_chrono_duration) { \
             std::cout << log_level_strings[level].prio << "[" << log_level_strings[level].name << "](tid=" << syscall(SYS_gettid) << ", dropped=" << num_dropped << ") " \
                       << args \
                       << std::endl; \
+            last_log_time = now; \
             num_dropped = 0; \
         } else { \
             num_dropped++; \
